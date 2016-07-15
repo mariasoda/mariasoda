543c2339a57d52f874f5f4dd6e3dafbaae973225
mariasoda <msodano@massart.edu>
2016-07-15 14:44:58 -0400

    crucial lamb updates

--- rhyme/app.js
+++ rhyme/app.js
@@ -9,13 +9,19 @@ $(document).ready(function(){
 		$(".lamb").text($("#friend-name").val());
 
 	});
-
-
+	
+	$("#lambpic").click(function(){
+		$("#baa").fadeIn
+	})
 
 	$("#lil").hover(function(){
-	$("#lil").animate({fontSize: "12px"}, 2000);
+		$("#lil").animate({fontSize: "12px"}, 2000);
 },
 	function(){
 		$("#lil").stop().animate({fontSize:"20px"});
 	});
-});
+
+	$("#appear").click(function(){
+		$("#school").fadeIn();
+		});
+	});	
