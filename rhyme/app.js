7fd7873ea9487692acd08f3bc429b3139089508d
mariasoda <msodano@massart.edu>
2016-07-17 23:58:56 -0400

    final page

--- rhyme/app.js
+++ rhyme/app.js
@@ -9,19 +9,32 @@ $(document).ready(function(){
 		$(".lamb").text($("#friend-name").val());
 
 	});
-	
-	$("#lambpic").click(function(){
-		$("#baa").fadeIn
-	})
+
+	$("#two").hover(function(){
+	$("#ptwo").fadeIn(4000);
+	});
+
+	$("#lambtwo").click(function(){
+	$("#ptwo").fadeIn(3000);
+	});
+
 
 	$("#lil").hover(function(){
 		$("#lil").animate({fontSize: "12px"}, 2000);
-},
+	},
 	function(){
 		$("#lil").stop().animate({fontSize:"20px"});
 	});
 
+		$("#followed").click(function(){
+		$("#hooves").fadeIn();
+		});
+
 	$("#appear").click(function(){
 		$("#school").fadeIn();
+		});
+
+		$("#last").click(function(){
+		$("#end").fadeIn();
 		});
 	});	
