d3bffc023777cc50b944de1275a3c1b535f8a7d1
mariasoda <msodano@massart.edu>
2016-10-29 23:49:16 -0400

    mobile test

--- final/home.js
+++ final/home.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 	if (windowsize > 640) {
 		$("#blue").hover(function(){
 			$("#blue").animate({height: "190px", width: "190px"}, 'fast');
+			$("#arrow").text("\u25BC", 'slow');
 			$("#myname").text("Maria Sodano", 'slow');
 			$("nav").show('slow');
 			});
 	}else{
 		$("nav").show(); }
+
 });
