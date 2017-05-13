2d549b6519e778813ab4dc97818f2825e1d64dcd
mariasoda <msodano@massart.edu>
2017-05-13 19:31:50 -0400

    test

--- final/home.js
+++ final/home.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var windowsize = $(window).width();
 	if (windowsize > 640) {
 		$("#blue").hover(function(){
-			$("#blue").animate({height: "190px", width: "190px"}, 'fast');
+			$("#blue").animate({height: 100%, width: 100%}, 'fast');
 			$("#arrow").text("\u25BC", 'slow');
 			$("#myname").text("Maria Sodano", 'slow');
 			$("nav").show('slow');
